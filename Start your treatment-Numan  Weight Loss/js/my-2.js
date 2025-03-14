0c36d41ca8ca6a4039d9d817849d143db8eddc18
USER <jashimrisda@gmail.com>
2025-03-14 22:41:05 +0600

    weightloss

---
+++ Start your treatment-Numan  Weight Loss/js/my-2.js
@@ -0,0 +1,6 @@
+
+// document.getElementById('asian').addEventListener('click', function() {
+//     // Redirect to a new page when the label is clicked
+//     window.location.href = "asian.html"; // Replace with your desired page URL
+//   });
+
---
+++ Start your treatment-Numan  Weight Loss/js/my.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+document.getElementById('nextPageLabel').addEventListener('click', function() {
+    // Redirect to a new page when the label is clicked
+    window.location.href = "Under18.html"; // Replace with your desired page URL
+  });
+
+
+
+
+
+document.getElementById('nextPageLab').addEventListener('click', function() {
+    // Redirect to a new page when the label is clicked
+    window.location.href = "18to74.html"; // Replace with your desired page URL
+  });
+
+
+
+
+document.getElementById('nextPageLabe').addEventListener('click', function() {
+    // Redirect to a new page when the label is clicked
+    window.location.href = "Under18.html"; // Replace with your desired page URL
+  });
+  
+
+
+
+
+
+
+
+
+    
+
+
+  document.getElementById('review-btn').addEventListener('click', function() {
+    alert('Redirecting to review your answers.');
+    
+});
+
+
+// 1
+
+
+
+
+
